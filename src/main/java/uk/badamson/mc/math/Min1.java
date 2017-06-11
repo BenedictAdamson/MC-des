582432a23bb8c248e5bd78f789739baa1d9ebf84
Benedict Adamson <badamson@spamcop.net>
2017-06-11 14:43:43 +0100

    Add (dummy) Min1 class.

---
+++ src/main/java/uk/badamson/mc/math/Min1.java
@@ -0,0 +1,14 @@
+package uk.badamson.mc.math;
+
+/**
+ * <p>
+ * Functions and auxiliary classes for one dimensional minimzation.
+ * </p>
+ */
+public final class Min1 {
+
+	private Min1() {
+		throw new AssertionError("Class should not be instantiated");
+	}
+
+}
---
+++ src/test/java/uk/badamson/mc/math/Min1Test.java
@@ -0,0 +1,10 @@
+package uk.badamson.mc.math;
+
+/**
+ * <p>
+ * Unit tests for the class {@link Min1}.
+ * </p>
+ */
+public class Min1Test {
+
+}
