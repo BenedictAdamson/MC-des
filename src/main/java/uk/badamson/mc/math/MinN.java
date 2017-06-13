d753153f3b20a2a1c66ec97993d5379083c976d9
Benedict Adamson <badamson@spamcop.net>
2017-06-13 21:46:18 +0100

    Add (dummy) class MinN.

---
+++ src/main/java/uk/badamson/mc/math/MinN.java
@@ -0,0 +1,14 @@
+package uk.badamson.mc.math;
+
+/**
+ * <p>
+ * Functions and auxiliary classes for minimization of a {@linkplain FunctionN
+ * multidimensional function}.
+ * </p>
+ */
+public final class MinN {
+
+	private MinN() {
+		throw new AssertionError("Class should not be instantiated");
+	}
+}
---
+++ src/test/java/uk/badamson/mc/math/MinNTest.java
@@ -0,0 +1,10 @@
+package uk.badamson.mc.math;
+
+/**
+ * <p>
+ * Unit tests of the {@link MinN} class.
+ * </p>
+ */
+public class MinNTest {
+
+}
