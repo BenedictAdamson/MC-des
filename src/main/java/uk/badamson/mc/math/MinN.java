7d78c9f221eca0b9c8c224c88394c4ebdbcc8acc
Benedict Adamson <badamson@spamcop.net>
2017-06-14 00:11:07 +0100

    Added math.MinN.createLineFunction

--- src/main/java/uk/badamson/mc/math/MinN.java
+++ src/main/java/uk/badamson/mc/math/MinN.java
@@ -1,5 +1,7 @@
 package uk.badamson.mc.math;
 
+import java.util.Objects;
+
 /**
  * <p>
  * Functions and auxiliary classes for minimization of a {@linkplain FunctionN
@@ -7,6 +9,73 @@ package uk.badamson.mc.math;
  * </p>
  */
 public final class MinN {
+
+	/**
+	 * <p>
+	 * Create a {@linkplain Function1 functor for a one-dimensional function of
+	 * a continuous variable} that is the evaluation of a {@linkplain FunctionN
+	 * functor for a multi-dimensional function of continuous variables} along a
+	 * given line.
+	 * </p>
+	 * <p>
+	 * The created functor retains references to the given objects. Those
+	 * objects should therefore not be changed while the created function is in
+	 * use.
+	 * </p>
+	 * 
+	 * @param f
+	 *            The multi-dimensional function
+	 * @param x0
+	 *            The origin point; the position in the space of the
+	 *            multidimensional function corresponding to the origin point of
+	 *            the created function.
+	 * @param dx
+	 *            The direction vector of the line in the space of the
+	 *            multidimensional function; (x + dx) corresponds to the value
+	 *            for 1.0 of the created function.
+	 * @return the created functor; not null.
+	 * 
+	 * @throws NullPointerException
+	 *             <ul>
+	 *             <li>If {@code f} is null.</li>
+	 *             <li>If {@code x0} is null.</li>
+	 *             <li>If {@code dx} is null.</li>
+	 *             </ul>
+	 * @throws IllegalArgumentException
+	 *             <ul>
+	 *             <li>If the length of {code x0} is 0.</li>
+	 *             <li>If the length of {code x0} is different from the length
+	 *             of {@code dx}.</li></li>
+	 *             <li>If the length of {code x0} is different from the
+	 *             {@linkplain FunctionN#getDimensions() number of dimensions}
+	 *             of {@code f}.</li></li>
+	 *             </ul>
+	 */
+	static Function1 createLineFunction(final FunctionN f, final double[] x0, final double[] dx) {
+		Objects.requireNonNull(f, "f");
+		Objects.requireNonNull(x0, "x0");
+		Objects.requireNonNull(dx, "dx");
+		final int n = x0.length;
+		if (n == 0) {
+			throw new IllegalArgumentException("x0.length == 0");
+		}
+		if (n != dx.length || n != f.getDimensions()) {
+			throw new IllegalArgumentException(
+					"Inconsistent lengths, x0 " + n + ", dx " + dx.length + ", f.dimensions " + f.getDimensions());
+		}
+
+		return new Function1() {
+
+			@Override
+			public double value(double w) {
+				final double[] x = new double[n];
+				for (int i = 0; i < n; i++) {
+					x[i] = x0[i] + w * dx[i];
+				}
+				return f.value(x);
+			}
+		};
+	}
 
 	private MinN() {
 		throw new AssertionError("Class should not be instantiated");
--- src/test/java/uk/badamson/mc/math/MinNTest.java
+++ src/test/java/uk/badamson/mc/math/MinNTest.java
@@ -1,10 +1,97 @@
 package uk.badamson.mc.math;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
 /**
  * <p>
  * Unit tests of the {@link MinN} class.
  * </p>
  */
 public class MinNTest {
+
+	private static final FunctionN CONSTANT_1 = new FunctionN() {
+
+		@Override
+		public int getDimensions() {
+			return 1;
+		}
+
+		@Override
+		public double value(double[] x) {
+			return 1.0;
+		}
+	};
+
+	private static final FunctionN BILINEANR_1 = new FunctionN() {
+
+		@Override
+		public int getDimensions() {
+			return 2;
+		}
+
+		@Override
+		public double value(double[] x) {
+			return x[0] + x[1];
+		}
+	};
+
+	private static Function1 createLineFunction(final FunctionN f, final double[] x0, final double[] dx) {
+		final Function1 lineFunction = MinN.createLineFunction(f, x0, dx);
+
+		assertNotNull("Not null, result", lineFunction);
+
+		return lineFunction;
+	}
+
+	@Test
+	public void createLineFunction_bilinearA() {
+		final double[] x0 = { 0.0, 0.0 };
+		final double[] dx = { 1.0, 0.0 };
+
+		final Function1 lineFunction = createLineFunction(BILINEANR_1, x0, dx);
+
+		assertEquals("lineFunction[0]", 0.0, lineFunction.value(0.0), 1E-3);
+		assertEquals("lineFunction[1.0]", 1.0, lineFunction.value(1.0), 1E-3);
+		assertEquals("lineFunction[-1.0]", -1.0, lineFunction.value(-1.0), 1E-3);
+	}
+
+	@Test
+	public void createLineFunction_bilinearB() {
+		final double[] x0 = { 0.0, 0.0 };
+		final double[] dx = { 0.0, 1.0 };
+
+		final Function1 lineFunction = createLineFunction(BILINEANR_1, x0, dx);
+
+		assertEquals("lineFunction[0]", 0.0, lineFunction.value(0.0), 1E-3);
+		assertEquals("lineFunction[1.0]", 1.0, lineFunction.value(1.0), 1E-3);
+		assertEquals("lineFunction[-1.0]", -1.0, lineFunction.value(-1.0), 1E-3);
+	}
+
+	@Test
+	public void createLineFunction_bilinearC() {
+		final double[] x0 = { 0.0, 0.0 };
+		final double[] dx = { 1.0, 1.0 };
+
+		final Function1 lineFunction = createLineFunction(BILINEANR_1, x0, dx);
+
+		assertEquals("lineFunction[0]", 0.0, lineFunction.value(0.0), 1E-3);
+		assertEquals("lineFunction[1.0]", 2.0, lineFunction.value(1.0), 1E-3);
+		assertEquals("lineFunction[-1.0]", -2.0, lineFunction.value(-1.0), 1E-3);
+	}
+
+	@Test
+	public void createLineFunction_constant() {
+		final double[] x0 = { 0.0 };
+		final double[] dx = { 1.0 };
+
+		final Function1 lineFunction = createLineFunction(CONSTANT_1, x0, dx);
+
+		assertEquals("lineFunction[0]", 1.0, lineFunction.value(0.0), 1E-3);
+		assertEquals("lineFunction[1.0]", 1.0, lineFunction.value(1.0), 1E-3);
+		assertEquals("lineFunction[-1.0]", 1.0, lineFunction.value(-1.0), 1E-3);
+	}
 
 }
