49b41960abb1ce9c2d5e8f02ee154b1324e44d9d
Benedict Adamson <badamson@spamcop.net>
2017-06-18 15:38:37 +0100

    Add (dummy) TimeStepEnergyErrorFunction.

---
+++ src/main/java/uk/badamson/mc/physics/TimeStepEnergyErrorFunction.java
@@ -0,0 +1,218 @@
+package uk.badamson.mc.physics;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import net.jcip.annotations.Immutable;
+import uk.badamson.mc.math.Function1WithGradientValue;
+import uk.badamson.mc.math.FunctionNWithGradient;
+import uk.badamson.mc.math.FunctionNWithGradientValue;
+import uk.badamson.mc.math.ImmutableVector;
+import uk.badamson.mc.math.MinN;
+
+/**
+ * <p>
+ * A {@linkplain FunctionNWithGradient functor} that calculates the physical
+ * modelling error of a system at a future point in time.</p
+ * <p>
+ * The function can be {@linkplain MinN minimised} to calculate the state of the
+ * physical system at that future point in time. By solving for a
+ * {@linkplain ImmutableVector multi-dimensional state vector}, that
+ * simultaneously solves for all the physical parameters of the system. By using
+ * minimisation to calculate the state, it is straightforward to include
+ * non-linear coupling of parameters of the physical system.
+ * </p>
+ * <p>
+ * The function also calculates the
+ * {@linkplain FunctionNWithGradientValue#getDfDx() rate of change} of the error
+ * with respect to changes in the state vector. That enables the minimisation to
+ * be performed using a
+ * {@linkplain MinN#findFletcherReevesPolakRibere(FunctionNWithGradient, ImmutableVector, double)
+ * conjugate-gradient method}. The conjugate-gradient method is likely to be
+ * efficient because the number of large eigen vectors of the state vector is
+ * likely to be far fewer than the number of physical parameters: if the system
+ * consists of loosely coupled objects, there is only one large eigen vector per
+ * object; if the system has some strongly coupled objects there will be one
+ * large eigen vector per vibration mode of the coupled system.
+ * </p>
+ * <p>
+ * The function calculates an error value that has dimensions of energy. It is
+ * therefore straightforward for the function to ensure conservation of energy.
+ * Other conservation laws can be incorporated by using appropriate dimension
+ * scales to convert other errors to energy errors.
+ * </p>
+ * <p>
+ * The function is {@linkplain #value(ImmutableVector) calculated} by summing
+ * the contributions of a {@linkplain #getTerms() collection of}
+ * {@linkplain TimeStepEnergyErrorFunction.Term terms}. Multiple physical
+ * processes and multiple objects can be modelled by including terms for each of
+ * the processes and objects.
+ * </p>
+ */
+@Immutable
+public final class TimeStepEnergyErrorFunction implements FunctionNWithGradient {
+
+	/**
+	 * <p>
+	 * A contributor to the {@linkplain TimeStepEnergyErrorFunction}physical
+	 * modelling error of a system at a future point in time}.
+	 * </p>
+	 */
+	@Immutable
+	public interface Term {
+
+	}// interface
+	private final ImmutableVector x0;
+	private final double dt;
+
+	private final List<Term> terms;
+
+	/**
+	 * <p>
+	 * Construct a functor that calculates the physical modelling error of a
+	 * system at a future point in time.
+	 * </p>
+	 *
+	 * <section>
+	 * <h1>Post Conditions</h1>
+	 * <ul>
+	 * <li>The constructed object has attribute and aggregate values equal to
+	 * the given values.</li>
+	 * </ul>
+	 * </section>
+	 *
+	 * @param x0
+	 *            The state vector of the physical system at the current point
+	 *            in time.
+	 * @param dt
+	 *            The size of the time-step; the difference between the futre
+	 *            point in time and the current point in time.
+	 * @param terms
+	 *            The terms that contribute to the
+	 *            {@linkplain #value(ImmutableVector) value} of this function.
+	 * 
+	 * @throws NullPointerException
+	 *             <ul>
+	 *             <li>If {@code x0} is null.</li>
+	 *             <li>If {@code terms} is null.</li>
+	 *             <li>If {@code terms} contains any null references.</li>
+	 * @throws IllegalArgumentException
+	 *             If {@code dt} is not positive and
+	 *             {@linkplain Double#isInfinite() finite}.
+	 */
+	public TimeStepEnergyErrorFunction(ImmutableVector x0, double dt, List<Term> terms) {
+		Objects.requireNonNull(x0, "x0");
+		Objects.requireNonNull(terms, "terms");
+		if (dt <= 0.0 || !Double.isFinite(dt)) {
+			throw new IllegalArgumentException("dt " + dt);
+		}
+
+		this.x0 = x0;
+		this.dt = dt;
+		this.terms = Collections.unmodifiableList(new ArrayList<>(terms));
+
+		/* Check precondition after construction to avoid race hazards. */
+		if (this.terms.contains(null)) {
+			throw new NullPointerException("null term");
+		}
+	}
+
+	/**
+	 * <p>
+	 * The number of independent variables of this function; the number of
+	 * variables of the physical model.
+	 * </p>
+	 * <ul>
+	 * <li>The dimension equals the {@linkplain ImmutableVector#getDimension()
+	 * dimension} of the {@linkplain #getX0() state vector of the physical
+	 * system at the current point in time}.</li>
+	 * </ul>
+	 * 
+	 * @return the number of dimensions; positive.
+	 */
+	@Override
+	public final int getDimension() {
+		return x0.getDimension();
+	}
+
+	/**
+	 * <p>
+	 * The size of the time-step; the difference between the futre point in time
+	 * and the current point in time.
+	 * </p>
+	 * <ul>
+	 * <li>The time-step is positive and {@linkplain Double#isInfinite()
+	 * finite}.</li>
+	 * </ul>
+	 *
+	 * @return the dt
+	 */
+	public final double getDt() {
+		return dt;
+	}
+
+	/**
+	 * <p>
+	 * The terms that contribute to the {@linkplain #value(ImmutableVector)
+	 * value} of this function.
+	 * </p>
+	 * <ul>
+	 * <li>Always have a (non null) collection of terms.</li>
+	 * <li>The collection of terms does not
+	 * {@linkplain Collection#contains(Object) contain} any null elements.</li>
+	 * <li>The collection of terms may include duplicates.</li>
+	 * <li>The collection of terms may be
+	 * {@linkplain Collections#unmodifiableCollection(Collection)
+	 * unmodifiable}.</li>
+	 * </ul>
+	 *
+	 * @return the terms
+	 */
+	public final List<Term> getTerms() {
+		return terms;
+	}
+
+	/**
+	 * <p>
+	 * The state vector of the physical system at the current point in time.
+	 * </p>
+	 * <ul>
+	 * <li>Always have a (non null) state vector of the physical system at the
+	 * current point in time.</li>
+	 * </ul>
+	 *
+	 * @return the state vector; not null.
+	 */
+	public final ImmutableVector getX0() {
+		return x0;
+	}
+
+	/**
+	 * <p>
+	 * Calculate the physical modelling error of the system at the
+	 * {@linkplain #getDt() future point in time}.
+	 * </p>
+	 * <ul>
+	 * <li>Always returns a (non null) value.</li>
+	 * <li>The {@linkplain Function1WithGradientValue#getX() domain value} of
+	 * the returned object is the given domain value.</li>
+	 * </ul>
+	 * 
+	 * @param x
+	 *            The state of the physical system at the future point in time
+	 * @return The value of the function.
+	 * @throws NullPointerException
+	 *             {@inheritDoc}
+	 * @throws IllegalArgumentException
+	 *             {@inheritDoc}
+	 */
+	@Override
+	public FunctionNWithGradientValue value(ImmutableVector x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/uk/badamson/mc/physics/TimeStepEnergyErrorFunctionTest.java
@@ -0,0 +1,125 @@
+package uk.badamson.mc.physics;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import uk.badamson.mc.ObjectTest;
+import uk.badamson.mc.math.FunctionNWithGradientTest;
+import uk.badamson.mc.math.FunctionNWithGradientValue;
+import uk.badamson.mc.math.ImmutableVector;
+
+/**
+ * <p>
+ * Unit tests for the class {@link TimeStepEnergyErrorFunction}.
+ * </p>
+ */
+public class TimeStepEnergyErrorFunctionTest {
+
+	private static final class Term0 implements TimeStepEnergyErrorFunction.Term {
+
+	}// class
+
+	/**
+	 * <p>
+	 * Unit tests classes that implement the interface
+	 * {@link TimeStepEnergyErrorFunction.Test}.
+	 * </p>
+	 */
+	public static class TermTest {
+
+		public static void assertInvariants(TimeStepEnergyErrorFunction.Term t) {
+			ObjectTest.assertInvariants(t);// inherited
+		}
+
+		public static void assertInvariants(TimeStepEnergyErrorFunction.Term t1, TimeStepEnergyErrorFunction.Term t2) {
+			ObjectTest.assertInvariants(t1, t2);// inherited
+		}
+	}// class
+
+	private static final double DT_A = 1.0;
+
+	private static final double DT_B = 1E-3;
+	private static final ImmutableVector X_1A = ImmutableVector.create(1.0);
+
+	private static final ImmutableVector X_1B = ImmutableVector.create(2.0);
+	private static final ImmutableVector X_2A = ImmutableVector.create(1.0, 2.0);
+	private static final ImmutableVector X_2B = ImmutableVector.create(3.0, 5.0);
+	private static final Term0 TERM_0A = new Term0();
+
+	private static final Term0 TERM_0B = new Term0();
+	public static void assertInvariants(TimeStepEnergyErrorFunction f) {
+		ObjectTest.assertInvariants(f);// inherited
+		FunctionNWithGradientTest.assertInvariants(f);// inherited
+
+		final ImmutableVector x0 = f.getX0();
+		final double dt = f.getDt();
+		final Collection<TimeStepEnergyErrorFunction.Term> terms = f.getTerms();
+
+		assertNotNull("Always have a state vector of the physical system at the current point in time.", x0);// guard
+		assertNotNull("Always have a collection of terms.", terms);// guard
+
+		assertTrue("The time-step <" + dt + "> is positive and finite.", 0.0 < dt && Double.isFinite(dt));
+		for (TimeStepEnergyErrorFunction.Term term : terms) {
+			assertNotNull("The collection of terms does not contain any null elements.", term);// guard
+			TermTest.assertInvariants(term);
+		}
+		assertEquals(
+				"The dimension equals the dimension of the state vector of the physical system at the current point in time.",
+				x0.getDimension(), f.getDimension());
+	}
+
+	public static void assertInvariants(TimeStepEnergyErrorFunction f1, TimeStepEnergyErrorFunction f2) {
+		ObjectTest.assertInvariants(f1, f2);// inherited
+	}
+
+	private static TimeStepEnergyErrorFunction constructor(ImmutableVector x0, double dt,
+			List<TimeStepEnergyErrorFunction.Term> terms) {
+		final TimeStepEnergyErrorFunction f = new TimeStepEnergyErrorFunction(x0, dt, terms);
+
+		assertInvariants(f);
+		assertSame("x0", x0, f.getX0());
+		assertEquals("dt", dt, f.getDt(), Double.MIN_NORMAL);
+		assertEquals("terms", terms, f.getTerms());
+
+		return f;
+	}
+
+	private static FunctionNWithGradientValue value(TimeStepEnergyErrorFunction f, ImmutableVector x) {
+		final FunctionNWithGradientValue fx = FunctionNWithGradientTest.value(f, x);// inherited
+
+		return fx;
+	}
+
+	@Test
+	public void constructor_A() {
+		final List<TimeStepEnergyErrorFunction.Term> terms = Collections.emptyList();
+		constructor(X_1A, DT_A, terms);
+	}
+
+	@Test
+	public void constructor_B() {
+		final List<TimeStepEnergyErrorFunction.Term> terms = Collections.singletonList(TERM_0A);
+		constructor(X_1A, DT_A, terms);
+	}
+
+	@Test
+	public void constructor_C() {
+		final List<TimeStepEnergyErrorFunction.Term> terms = Arrays.asList(TERM_0A, TERM_0B);
+		constructor(X_1A, DT_B, terms);
+	}
+
+	@Test
+	public void constructor_D() {
+		final List<TimeStepEnergyErrorFunction.Term> terms = Arrays.asList(TERM_0A);
+		constructor(X_2A, DT_B, terms);
+	}
+}
