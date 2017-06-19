26625559c49339437a37d6cd41dc7608e3dffb7d
Benedict Adamson <badamson@spamcop.net>
2017-06-19 22:18:06 +0100

    opy files as basis for ABC AbstractTimeStepEnergyErrorFunctionTermTest

---
+++ src/main/java/uk/badamson/mc/physics/AbstractTimeStepEnergyErrorFunctionTerm.java
@@ -0,0 +1,331 @@
+package uk.badamson.mc.physics;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import uk.badamson.mc.math.ImmutableVector;
+import uk.badamson.mc.physics.TimeStepEnergyErrorFunction;
+import uk.badamson.mc.physics.TimeStepEnergyErrorFunctionTerm;;
+
+/**
+ * <p>
+ * A {@linkplain TimeStepEnergyErrorFunctionTerm term} for a
+ * {@linkplain TimeStepEnergyErrorFunction functor that calculates the physical
+ * modelling error of a system at a future point in time} that gives the degree
+ * of inconsistency of the velocity and acceleration of a body.
+ * </p>
+ * <p>
+ * This term calculates the error in only one {@linkplain #getDirection()
+ * direction}. The error functor should therefore include a
+ * {@linkplain TimeStepEnergyErrorFunction#getTerms() term} for each dimension
+ * of space (usually 3) for each body. The directions for each body must not be
+ * co-linear.
+ * </p>
+ */
+public final class AbstractTimeStepEnergyErrorFunctionTerm implements TimeStepEnergyErrorFunctionTerm {
+
+	private static int[] copyTermIndex(int[] index) {
+		final int[] copy = Arrays.copyOf(index, index.length);
+		/* Check precondition after copy to avoid race hazards. */
+		for (int i : copy) {
+			if (i < 0) {
+				throw new IllegalArgumentException("Negative index term " + i);
+			}
+		}
+		return copy;
+	}
+
+	private static ImmutableVector extract(ImmutableVector x, int term[]) {
+		final int n = term.length;
+		final double[] extract = new double[n];
+		for (int i = 0; i < n; i++) {
+			extract[i] = x.get(term[i]);
+		}
+		return ImmutableVector.create(extract);
+	}
+
+	private final ImmutableVector direction;
+	private final double mass;
+	private final int[] velocityTerm;
+	private final int[] accelerationTerm;
+
+	/**
+	 * <p>
+	 * Construct a velocity error term.
+	 * </p>
+	 *
+	 * <section>
+	 * <h1>Post Conditions</h1>
+	 * <ul>
+	 * <li>The constructed object has attribute values equal to the given
+	 * values.</li>
+	 * </ul>
+	 * </section>
+	 *
+	 * @param direction
+	 *            The direction in which to calculate the error term.
+	 * @param mass
+	 *            A reference mass scale.
+	 * @param velocityTerm
+	 *            Which terms in the solution space vector correspond to the
+	 *            components of the velocity vector of the body.
+	 *            {@code velocityTerm[i]} is index of component <var>i</var>,
+	 * @param accelerationTerm
+	 *            Which terms in the solution space vector correspond to the
+	 *            components of the acceleration vector of the body.
+	 *            {@code velocityTerm[i]} is index of component <var>i</var>,
+	 * 
+	 * @throws NullPointerException
+	 *             <ul>
+	 *             <li>If {@code direction} is null.</li>
+	 *             <li>If {@code velocityTerm} is null.</li>
+	 *             <li>If {@code accelerationTerm} is null.</li>
+	 * @throws IllegalArgumentException
+	 *             <ul>
+	 *             <li>If {@code direction} is not a unit vector.</li>
+	 *             <li>If {@code mass} is not a positive and
+	 *             {@linkplain Double#isFinite(double) finite}.</li>
+	 *             <li>If the length of {@code velocityTerm} does not equal the
+	 *             {@linkplain ImmutableVector#getDimension() dimension} of
+	 *             {@code direction}.</li>
+	 *             <li>If the length of {@code accelerationTerm} does not equal
+	 *             the dimension of {@code direction}.</li>
+	 *             <li>If {@code velocityTerm} has any negative values.</li>
+	 *             <li>If {@code accelerationTerm} has any negative values.</li>
+	 *             </ul>
+	 */
+	public AbstractTimeStepEnergyErrorFunctionTerm(ImmutableVector direction, double mass, int[] velocityTerm, int[] accelerationTerm) {
+		Objects.requireNonNull(direction, "direction");
+		Objects.requireNonNull(velocityTerm, "velocityTerm");
+		Objects.requireNonNull(accelerationTerm, "accelerationTerm");
+
+		final int n = direction.getDimension();
+		if (velocityTerm.length != n) {
+			throw new IllegalArgumentException("Inconsistent velocityTerm.length <" + velocityTerm.length
+					+ "> and direction.dimension<" + n + ">");
+		}
+		if (accelerationTerm.length != n) {
+			throw new IllegalArgumentException("Inconsistent accelerationTerm.length <" + accelerationTerm.length
+					+ "> and direction.dimension<" + n + ">");
+		}
+
+		this.direction = direction;
+		this.mass = mass;
+		this.velocityTerm = copyTermIndex(velocityTerm);
+		this.accelerationTerm = copyTermIndex(accelerationTerm);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * <ol>
+	 * <li>The method uses the {@linkplain #getVelocityTerm(int) velocity term
+	 * index} information and {@linkplain #getAccelerationTerm(int) acceleration
+	 * term index} information to extract velocity and acceleraation vectors
+	 * from the given state vectors.</li>
+	 * <li>It reduces those vectors to scalars by finding their components along
+	 * the {@linkplain #getDirection() direction vector}.</li>
+	 * <li>It calculates a mean acceleration from the old and new acceleration
+	 * values.</li>
+	 * <li>It calculates the extrapolated velocity from the old velocity and the
+	 * mean acceleration.</li>
+	 * <li>It calculates a velocity error by comparing the new velocity with the
+	 * extrapolated velocity.</li>
+	 * <li>From that it calculates an equivalent kinetic energy error, using the
+	 * {@linkplain #getMass() characteristic mass value}. That is the error term
+	 * it returns.</li>
+	 * </ol>
+	 * 
+	 * @param dedx
+	 *            {@inheritDoc}
+	 * @param state0
+	 *            {@inheritDoc}
+	 * @param state
+	 *            {@inheritDoc}
+	 * @param dt
+	 *            {@inheritDoc}
+	 * @return the value
+	 * 
+	 * @throws NullPointerException
+	 *             {@inheritDoc}
+	 * @throws IllegalArgumentException
+	 *             {@inheritDoc}
+	 * @throws IllegalArgumentException
+	 *             If the length of {@code dedx} does not equal the
+	 *             {@linkplain ImmutableVector#getDimension() dimension} of
+	 *             {@code state0}.
+	 */
+	@Override
+	public final double evaluate(double[] dedx, ImmutableVector state0, ImmutableVector state, double dt) {
+		Objects.requireNonNull(dedx, "dedx");
+		Objects.requireNonNull(state0, "x0");
+		Objects.requireNonNull(state, "x");
+		if (!(0.0 < dt && Double.isFinite(dt))) {
+			throw new IllegalArgumentException("dt " + dt);
+		}
+		final int nState = state0.getDimension();
+		if (state.getDimension() != nState) {
+			throw new IllegalArgumentException(
+					"Inconsistent dimensions x0 " + nState + " and x " + state.getDimension());
+		}
+		if (dedx.length != nState) {
+			throw new IllegalArgumentException(
+					"Inconsistent length of dedx " + dedx.length + " and dimension of x0 " + nState);
+		}
+
+		final ImmutableVector v0 = extract(state0, velocityTerm);
+		final ImmutableVector a0 = extract(state0, accelerationTerm);
+		final ImmutableVector v = extract(state, velocityTerm);
+		final ImmutableVector a = extract(state, accelerationTerm);
+		final ImmutableVector dv = v.minus(v0);
+		final ImmutableVector da = a.minus(a0);
+
+		final double a01 = a0.dot(direction);
+		final double dv1 = dv.dot(direction);
+		final double da1 = da.dot(direction);
+
+		final double aMean = a01 + 0.5 * da1;
+		final double dvExtrapolated = aMean * dt;
+
+		final double vError = dv1 - dvExtrapolated;
+		final double mvError = vError * mass;
+		final double eError = 0.5 * vError * mvError;
+
+		final double dedv = mvError;
+		final double deda = -0.5 * mvError * dt;
+
+		for (int i = 0, n = velocityTerm.length; i < n; ++i) {
+			final double dsdxi = direction.get(i);
+			final double dedvi = dedv * dsdxi;
+			final double dedai = deda * dsdxi;
+
+			dedx[velocityTerm[i]] += dedvi;
+			dedx[accelerationTerm[i]] += dedai;
+		}
+		return eError;
+	}
+
+	/**
+	 * <p>
+	 * Which terms in the solution space vector correspond to the components of
+	 * the acceleraation vector of the body.
+	 * </p>
+	 * 
+	 * @param i
+	 *            The component of interest.
+	 * @return the index of the component of the acceleration vector; not
+	 *         negative
+	 * 
+	 * @throws IndexOutOfBoundsException
+	 *             <ul>
+	 *             <li>If {@code i} is negative.</li>
+	 *             <li>If {@code i} is not less than the
+	 *             {@linkplain #getSpaceDimension() space dimension}.</li>
+	 *             </ul>
+	 */
+	public final int getAccelerationTerm(int i) {
+		return accelerationTerm[i];
+	}
+
+	/**
+	 * <p>
+	 * The direction in which to calculate the error term.
+	 * </p>
+	 * <ul>
+	 * <li>Always have a (non null) direction.</li>
+	 * <li>The direction vector is a unit vector (it has
+	 * {@linkplain ImmutableVector#magnitude() magnitude} 1.0).</li>
+	 * </ul>
+	 * 
+	 * @return the direction vector.
+	 */
+	public final ImmutableVector getDirection() {
+		return direction;
+	}
+
+	/**
+	 * <p>
+	 * A reference mass scale.
+	 * </p>
+	 * <p>
+	 * The functor uses this value to convert a position error into an energy
+	 * error. It is tempting to use the mass of the solid body for which this
+	 * functor calculates the position error, but that will produce bad results
+	 * if there are multiple bodies and they have very different masses; it is
+	 * better to use the same value for all bodies, with that value equal to the
+	 * mass of a typical body.
+	 * </p>
+	 * 
+	 * @return the mass; positive and {@linkplain Double#isFinite(double)
+	 *         finite}
+	 */
+	public final double getMass() {
+		return mass;
+	}
+
+	/**
+	 * <p>
+	 * The number of space dimensions for which this calculates a velocity
+	 * error.
+	 * </p>
+	 * <ul>
+	 * <li>The number of space dimensions is equal to the
+	 * {@linkplain ImmutableVector#getDimension() dimension} of the
+	 * {@linkplain #getDirection() direction vector} along which this calculates
+	 * the velocity error.
+	 * </ul>
+	 * 
+	 * @return
+	 */
+	public final int getSpaceDimension() {
+		return direction.getDimension();
+	}
+
+	/**
+	 * <p>
+	 * Which terms in the solution space vector correspond to the components of
+	 * the velocity vector of the body.
+	 * </p>
+	 * 
+	 * @param i
+	 *            The component of interest.
+	 * @return the index of the component of the velocity vector; not negative
+	 * 
+	 * @throws IndexOutOfBoundsException
+	 *             <ul>
+	 *             <li>If {@code i} is negative.</li>
+	 *             <li>If {@code i} is not less than the
+	 *             {@linkplain #getSpaceDimension() space dimension}.</li>
+	 *             </ul>
+	 */
+	public final int getVelocityTerm(int i) {
+		return velocityTerm[i];
+	}
+
+	/**
+	 * <p>
+	 * Whether this term can be calculated for a physical state vector that has
+	 * a given number of variables.
+	 * </p>
+	 * <ul>
+	 * <li>This is valid for a given dimension if, and only if, the number of
+	 * variables exceeds the largest {@linkplain #getVelocityTerm(int) velocity
+	 * term index} and exceeds the largest {@linkplain #getAccelerationTerm(int)
+	 * acceleration term index}.</li>
+	 * </ul>
+	 * 
+	 * @return whether valid.
+	 * @throws IllegalArgumentException
+	 *             If {@code n} is not positive.
+	 */
+	@Override
+	public boolean isValidForDimension(int n) {
+		for (int i = 0, pn = velocityTerm.length; i < pn; ++i) {
+			if (n < velocityTerm[i] + 1 || n < accelerationTerm[i] + 1) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
---
+++ src/test/java/uk/badamson/mc/physics/AbstractTimeStepEnergyErrorFunctionTermTest.java
@@ -0,0 +1,417 @@
+package uk.badamson.mc.physics;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import uk.badamson.mc.ObjectTest;
+import uk.badamson.mc.math.ImmutableVector;
+import uk.badamson.mc.physics.TimeStepEnergyErrorFunctionTermTest;
+
+/**
+ * <p>
+ * Unit tests for the class {@link AbstractTimeStepEnergyErrorFunctionTerm}.
+ * </p>
+ */
+public class AbstractTimeStepEnergyErrorFunctionTermTest {
+
+	private static final double TOLERANCE_1 = Math.nextAfter(1.0, Double.POSITIVE_INFINITY) - 1.0;
+
+	public static void assertInvariants(AbstractTimeStepEnergyErrorFunctionTerm term) {
+		ObjectTest.assertInvariants(term);// inherited
+		TimeStepEnergyErrorFunctionTermTest.assertInvariants(term);// inherited
+
+		final ImmutableVector direction = term.getDirection();
+		final double mass = term.getMass();
+		final int spaceDimension = term.getSpaceDimension();
+
+		assertNotNull("Not null, direction", direction);// guard
+
+		assertEquals("The direction vector is a unit vector", 1.0, direction.magnitude2(),
+				Math.sqrt(TOLERANCE_1 * spaceDimension));
+		assertEquals("The number of space dimensions is equal to the dimension of the direction vector.",
+				direction.getDimension(), spaceDimension);
+		assertTrue("Mass <" + mass + "> is positive and  finite", 0.0 < mass && Double.isFinite(mass));
+
+		int maxTerm = -1;
+		for (int i = 0; i < spaceDimension; i++) {
+			final int velocityTerm = term.getVelocityTerm(i);
+			final int accelerationTerm = term.getAccelerationTerm(i);
+			assertTrue("velocityTerm[" + i + "] <" + velocityTerm + "> not negative", 0 <= velocityTerm);
+			assertTrue("accelerationTerm[" + i + "] <" + accelerationTerm + "> not negative", 0 <= accelerationTerm);
+			maxTerm = Math.max(maxTerm, velocityTerm);
+			maxTerm = Math.max(maxTerm, accelerationTerm);
+		}
+		assertTrue("This is valid for a dimension if, and only if, "
+				+ "the number of variables exceeds the largest velocity term index and "
+				+ "exceeds the largest acceleration term index", term.isValidForDimension(maxTerm + 1));
+	}
+
+	public static void assertInvariants(AbstractTimeStepEnergyErrorFunctionTerm term1, AbstractTimeStepEnergyErrorFunctionTerm term2) {
+		ObjectTest.assertInvariants(term1, term2);// inherited
+		TimeStepEnergyErrorFunctionTermTest.assertInvariants(term1, term2);// inherited
+	}
+
+	private static AbstractTimeStepEnergyErrorFunctionTerm constructor(ImmutableVector direction, double mass, int[] velocityTerm,
+			int[] accelerationTerm) {
+		final AbstractTimeStepEnergyErrorFunctionTerm term = new AbstractTimeStepEnergyErrorFunctionTerm(direction, mass, velocityTerm, accelerationTerm);
+
+		assertInvariants(term);
+
+		assertSame("direction", direction, term.getDirection());
+		assertEquals("mass", mass, term.getMass(), Double.MIN_NORMAL);
+		for (int i = 0, n = direction.getDimension(); i < n; ++i) {
+			assertEquals("velocityTerm[" + i + "]", velocityTerm[i], term.getVelocityTerm(i));
+			assertEquals("accelerationTerm[" + i + "]", accelerationTerm[i], term.getAccelerationTerm(i));
+		}
+
+		return term;
+	}
+
+	private static double evaluate(AbstractTimeStepEnergyErrorFunctionTerm term, double[] dedx, ImmutableVector x0, ImmutableVector x,
+			double dt) {
+		final double e = TimeStepEnergyErrorFunctionTermTest.evaluate(term, dedx, x0, x, dt);
+
+		assertInvariants(term);
+
+		return e;
+	}
+
+	private static final void evaluate_1(double direction, double mass, double dedv0, double deda0, double v0,
+			double a0, double v, double a, double dt, double eExpected, double dEDVExpected, double dEDAExpected,
+			double tolerance) {
+		final int velocityTerm = 0;
+		final int accelerationTerm = 1;
+		final double[] dedx = { dedv0, deda0 };
+		final ImmutableVector state0 = ImmutableVector.create(v0, a0);
+		final ImmutableVector state = ImmutableVector.create(v, a);
+		final AbstractTimeStepEnergyErrorFunctionTerm term = new AbstractTimeStepEnergyErrorFunctionTerm(ImmutableVector.create(direction), mass,
+				new int[] { velocityTerm }, new int[] { accelerationTerm });
+
+		final double e = evaluate(term, dedx, state0, state, dt);
+
+		assertEquals("energy", eExpected, e, tolerance);
+		assertEquals("dedx[velocityTerm]", dEDVExpected, dedx[velocityTerm], tolerance);
+		assertEquals("dedx[accelerationTerm]", dEDAExpected, dedx[accelerationTerm], tolerance);
+	}
+
+	private static final void evaluate_1Minimum(double direction, double mass, double dedv0, double deda0, double v0,
+			double a0, double v, double a, double dt, double tolerance) {
+		final double eExpected = 0.0;
+		final double dEDVExpected = dedv0;
+		final double dEDAExpected = deda0;
+
+		evaluate_1(direction, mass, dedv0, deda0, v0, a0, v, a, dt, eExpected, dEDVExpected, dEDAExpected, tolerance);
+	}
+
+	@Test
+	public void constructor_1A() {
+		final ImmutableVector direction = ImmutableVector.create(1.0);
+		final double mass = 2.0;
+		final int[] positionTerm = { 3 };
+		final int[] velocityTerm = { 4 };
+
+		constructor(direction, mass, positionTerm, velocityTerm);
+	}
+
+	@Test
+	public void constructor_1B() {
+		final ImmutableVector direction = ImmutableVector.create(-1.0);
+		final double mass = 1E24;
+		final int[] positionTerm = { 7 };
+		final int[] velocityTerm = { 11 };
+
+		constructor(direction, mass, positionTerm, velocityTerm);
+	}
+
+	@Test
+	public void constructor_2() {
+		final double e = Math.sqrt(2.0) / 2.0;
+		final ImmutableVector direction = ImmutableVector.create(e, e);
+		final double mass = 1000.0;
+		final int[] positionTerm = { 3, 4 };
+		final int[] velocityTerm = { 5, 6 };
+
+		constructor(direction, mass, positionTerm, velocityTerm);
+	}
+
+	@Test
+	public void evaluate_1A() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0.0;
+		final double v0 = 0.0;
+		final double a0 = 0.0;
+		final double v = 0.0;
+		final double a = 2.0;
+		final double dt = 1.0;
+		final double eExpected = 0.5;
+		final double dEDVExpected = -1.0;
+		final double dEDAExpected = 0.5;
+		final double tolerance = 1E-3;
+
+		evaluate_1(direction, mass, dedv0, deda0, v0, a0, v, a, dt, eExpected, dEDVExpected, dEDAExpected, tolerance);
+	}
+
+	@Test
+	public void evaluate_1A0() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0.0;
+		final double v0 = 0.0;
+		final double a0 = 2.0;
+		final double v = 0.0;
+		final double a = 0.0;
+		final double dt = 1.0;
+		final double eExpected = 0.5;
+		final double dEDVExpected = -1.0;
+		final double dEDAExpected = 0.5;
+		final double tolerance = 1E-3;
+
+		evaluate_1(direction, mass, dedv0, deda0, v0, a0, v, a, dt, eExpected, dEDVExpected, dEDAExpected, tolerance);
+	}
+
+	@Test
+	public void evaluate_1DT() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0.0;
+		final double v0 = 0.0;
+		final double a0 = 0.0;
+		final double v = 0.0;
+		final double a = 2.0;
+		final double dt = 2.0;
+		final double eExpected = 2.0;
+		final double dEDVExpected = -2.0;
+		final double dEDAExpected = 2.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1(direction, mass, dedv0, deda0, v0, a0, v, a, dt, eExpected, dEDVExpected, dEDAExpected, tolerance);
+	}
+
+	@Test
+	public void evaluate_1MinimumA0A() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0;
+		final double v0 = 0.0;
+		final double a0 = 2.0;
+		final double v = 0.0;
+		final double a = -2.0;
+		final double dt = 1.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1Minimum(direction, mass, dedv0, deda0, v0, a0, v, a, dt, tolerance);
+	}
+
+	@Test
+	public void evaluate_1MinimumA0V() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0;
+		final double v0 = 0.0;
+		final double a0 = 2.0;
+		final double v = 1.0;
+		final double a = 0.0;
+		final double dt = 1.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1Minimum(direction, mass, dedv0, deda0, v0, a0, v, a, dt, tolerance);
+	}
+
+	@Test
+	public void evaluate_1MinimumBase() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0;
+		final double v0 = 0.0;
+		final double a0 = 0.0;
+		final double v = 0.0;
+		final double a = 0.0;
+		final double dt = 1.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1Minimum(direction, mass, dedv0, deda0, v0, a0, v, a, dt, tolerance);
+	}
+
+	@Test
+	public void evaluate_1MinimumDEDA0() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 2.0;
+		final double v0 = 0.0;
+		final double a0 = 0.0;
+		final double v = 0.0;
+		final double a = 0.0;
+		final double dt = 1.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1Minimum(direction, mass, dedv0, deda0, v0, a0, v, a, dt, tolerance);
+	}
+
+	@Test
+	public void evaluate_1MinimumDEDV0() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 2.0;
+		final double deda0 = 0.0;
+		final double v0 = 0.0;
+		final double a0 = 0.0;
+		final double v = 0.0;
+		final double a = 0.0;
+		final double dt = 1.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1Minimum(direction, mass, dedv0, deda0, v0, a0, v, a, dt, tolerance);
+	}
+
+	@Test
+	public void evaluate_1MinimumDirection() {
+		final double direction = -1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0.0;
+		final double v0 = 0.0;
+		final double a0 = 0.0;
+		final double v = 0.0;
+		final double a = 0.0;
+		final double dt = 1.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1Minimum(direction, mass, dedv0, deda0, v0, a0, v, a, dt, tolerance);
+	}
+
+	@Test
+	public void evaluate_1MinimumMass() {
+		final double direction = 1.0;
+		final double mass = 2.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0.0;
+		final double v0 = 0.0;
+		final double a0 = 0.0;
+		final double v = 0.0;
+		final double a = 0.0;
+		final double dt = 1.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1Minimum(direction, mass, dedv0, deda0, v0, a0, v, a, dt, tolerance);
+	}
+
+	@Test
+	public void evaluate_1MinimumV0A() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0;
+		final double v0 = 2.0;
+		final double a0 = 0.0;
+		final double v = 0.0;
+		final double a = -4.0;
+		final double dt = 1.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1Minimum(direction, mass, dedv0, deda0, v0, a0, v, a, dt, tolerance);
+	}
+
+	@Test
+	public void evaluate_1MinimumV0V() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0;
+		final double v0 = 2.0;
+		final double a0 = 0.0;
+		final double v = 2.0;
+		final double a = 0.0;
+		final double dt = 1.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1Minimum(direction, mass, dedv0, deda0, v0, a0, v, a, dt, tolerance);
+	}
+
+	@Test
+	public void evaluate_1V() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0.0;
+		final double v0 = 0.0;
+		final double a0 = 0.0;
+		final double v = 2.0;
+		final double a = 0.0;
+		final double dt = 1.0;
+		final double eExpected = 2.0;
+		final double dEDVExpected = 2.0;
+		final double dEDAExpected = -1.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1(direction, mass, dedv0, deda0, v0, a0, v, a, dt, eExpected, dEDVExpected, dEDAExpected, tolerance);
+	}
+
+	@Test
+	public void evaluate_1V0() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0.0;
+		final double v0 = 2.0;
+		final double a0 = 0.0;
+		final double v = 0.0;
+		final double a = 0.0;
+		final double dt = 1.0;
+		final double eExpected = 2.0;
+		final double dEDVExpected = -2.0;
+		final double dEDAExpected = 1.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1(direction, mass, dedv0, deda0, v0, a0, v, a, dt, eExpected, dEDVExpected, dEDAExpected, tolerance);
+	}
+
+	@Test
+	public void evaluate_1VDT() {
+		final double direction = 1.0;
+		final double mass = 1.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0.0;
+		final double v0 = 0.0;
+		final double a0 = 0.0;
+		final double v = 2.0;
+		final double a = 0.0;
+		final double dt = 2.0;
+		final double eExpected = 2.0;
+		final double dEDVExpected = 2.0;
+		final double dEDAExpected = -2.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1(direction, mass, dedv0, deda0, v0, a0, v, a, dt, eExpected, dEDVExpected, dEDAExpected, tolerance);
+	}
+
+	@Test
+	public void evaluate_1VM() {
+		final double direction = 1.0;
+		final double mass = 2.0;
+		final double dedv0 = 0.0;
+		final double deda0 = 0.0;
+		final double v0 = 0.0;
+		final double a0 = 0.0;
+		final double v = 2.0;
+		final double a = 0.0;
+		final double dt = 1.0;
+		final double eExpected = 4.0;
+		final double dEDVExpected = 4.0;
+		final double dEDAExpected = -2.0;
+		final double tolerance = 1E-3;
+
+		evaluate_1(direction, mass, dedv0, deda0, v0, a0, v, a, dt, eExpected, dEDVExpected, dEDAExpected, tolerance);
+	}
+}
