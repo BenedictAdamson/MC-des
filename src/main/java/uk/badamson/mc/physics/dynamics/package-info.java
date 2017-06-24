3a3857b9093f1b6e34e508b7e536cd20683c6340
Benedict Adamson <badamson@spamcop.net>
2017-06-24 10:50:28 +0100

    Add package uk.badamson.mc.physics.dynamics

---
+++ src/main/java/uk/badamson/mc/physics/dynamics/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <p>
+ * {@linkplain uk.badamson.mc.physics.TimeStepEnergyErrorFunctionTerm Terms} for
+ * a {@linkplain uk.badamson.mc.physics.TimeStepEnergyErrorFunction functor that
+ * calculates the physical modelling error of a system at a future point in
+ * time} that will result in solution of the dynamics of the system if the
+ * function is {@linkplain uk.badamson.mc.math.MinN minimised}.
+ * </p>
+ */
+package uk.badamson.mc.physics.dynamics;
