5bd125a91c1b79a4b9c4f7dd15bf41bae559a4fd
Benedict Adamson <badamson@spamcop.net>
2017-12-20 15:54:17 +0000

    Add Command interface

---
+++ src/main/java/uk/badamson/mc/actor/Command.java
@@ -0,0 +1,16 @@
+package uk.badamson.mc.actor;
+
+/**
+ * <p>
+ * A message that an {@linkplain Actor actor} might send through a
+ * {@linkplain Medium medium} to other actors,
+ * which an actor (or actors) should interpret as a command issued to them by the sender.
+ * </p>
+ * <p>
+ * Objects of this class are
+ * therefore means by which actors can express orders to sub-ordinates.
+ * </p>
+ */
+public interface Command extends Message {
+
+}
