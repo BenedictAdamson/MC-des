c0faaa0fba96590b16adf486b06b771c8330892a
Benedict Adamson <badamson@spamcop.net>
2018-02-04 12:52:20 +0000

    A Game CONTAINS persons.

--- src/main/java/uk/badamson/mc/Game.java
+++ src/main/java/uk/badamson/mc/Game.java
@@ -1,9 +1,13 @@
 package uk.badamson.mc;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import uk.badamson.mc.actor.ActorInterface;
 import uk.badamson.mc.simulation.Clock;
+import uk.badamson.mc.simulation.Person;
 
 /**
  * <p>
@@ -16,6 +20,7 @@ import uk.badamson.mc.simulation.Clock;
 public final class Game {
 
     private final Clock clock = new Clock(TimeUnit.MILLISECONDS, 0L);
+    private final Set<Person> persons = new HashSet<>();
     private ActorInterface playerActorInterface;
 
     /**
@@ -43,12 +48,34 @@ public final class Game {
 
     /**
      * <p>
+     * The simulated persons in this game instance.
+     * </p>
+     * <ul>
+     * <li>Always have a (non null) set of persons.</li>
+     * <li>The set of persons does not have a null element.</li>
+     * <li>The set of persons is not publicly
+     * {@linkplain Collections#unmodifiableSet(Set) modifiable}.</li>
+     * </ul>
+     * 
+     * @return the persons
+     */
+    public final Set<Person> getPersons() {
+        return persons;
+    }
+
+    /**
+     * <p>
      * The API (service interface) through which the human player of this game
      * effect changes to the simulation of the person they are playing.
      * </p>
+     * <ul>
+     * <li>The player actor interface is null if the player is not (yet) playing a
+     * particular person.</li>
+     * <li>If the player actor interface is not null, it is one of the
+     * {@linkplain #getPersons() simulated persons} of this game.</li>
+     * </ul>
      * 
-     * @return the interface; or null if the player is not (yet) playing a
-     *         particular person.
+     * @return the interface.
      */
     public final ActorInterface getPlayerActorInterface() {
         return playerActorInterface;
--- src/test/java/uk/badamson/mc/GameTest.java
+++ src/test/java/uk/badamson/mc/GameTest.java
@@ -1,11 +1,17 @@
 package uk.badamson.mc;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
 
 import org.junit.Test;
 
+import uk.badamson.mc.actor.ActorInterface;
 import uk.badamson.mc.simulation.Clock;
 import uk.badamson.mc.simulation.ClockTest;
+import uk.badamson.mc.simulation.Person;
+import uk.badamson.mc.simulation.PersonTest;
 
 /**
  * <p>
@@ -18,9 +24,19 @@ public class GameTest {
         ObjectTest.assertInvariants(game);// inherited
 
         final Clock clock = game.getClock();
+        final Set<Person> persons = game.getPersons();
+        final ActorInterface playerActorInterface = game.getPlayerActorInterface();
+
         assertNotNull("Not null, clock", clock);// guard
+        assertNotNull("Always have a set of persons.", persons);// guard
 
         ClockTest.assertInvariants(clock);
+        for (Person person : persons) {
+            assertNotNull("The set of persons does not have a null element.", person);// guard
+            PersonTest.assertInvariants(person);
+        }
+        assertTrue("If the player actor interface is not null, it is one of the simulated persons of this game.",
+                playerActorInterface == null || persons.contains(playerActorInterface));
     }
 
     public static void assertInvariants(Game game1, Game game2) {
