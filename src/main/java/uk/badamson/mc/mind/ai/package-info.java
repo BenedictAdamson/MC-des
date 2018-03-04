51eb1557a1b61e00ec2b4565d99842f227032bb1
Benedict Adamson <badamson@spamcop.net>
2018-03-04 11:27:42 +0000

    Add AI package.

---
+++ src/main/java/uk/badamson/mc/mind/ai/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>
+ * Artificial Intelligence code for providing
+ * {@linkplain uk.badamson.mc.simulation.Person simulated persons} with a
+ * {@linkplain uk.badamson.mc.mind.Mind mind}.
+ * </p>
+ */
+package uk.badamson.mc.mind.ai;
