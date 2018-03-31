f7854491bd9a85f18d61e6b71ab029b136f32431
Benedict Adamson <badamson@spamcop.net>
2018-03-31 12:04:15 +0100

    Add ObjectStateSpaceMapper interface.

---
+++ src/main/java/uk/badamson/mc/physics/ObjectStateSpaceMapper.java
@@ -0,0 +1,52 @@
+package uk.badamson.mc.physics;
+
+import net.jcip.annotations.Immutable;
+import uk.badamson.mc.math.ImmutableVectorN;
+
+/**
+ * <p>
+ * A Strategy for mapping from an object representation to (part of) a
+ * state-space representation, and vice versa.
+ * </p>
+ */
+@Immutable
+public interface ObjectStateSpaceMapper<OBJECT> {
+
+    /**
+     * <p>
+     * Using this mapping to convert an object to (part of) a state-space vector.
+     * </p>
+     * <ul>
+     * <li>The method sets (assigns) components of the state-space vector.</li>
+     * <li>This is the inverse operation of the {@link #toObject(ImmutableVectorN)}
+     * method.
+     * </ul>
+     * 
+     * @param state
+     *            The components of the state-space vector.
+     * @param object
+     *            The object to convert to state-space representation.
+     * @throws NullPointerException
+     *             If {@code state} is null.
+     * @throws IndexOutOfBoundsException
+     *             If {@code state} does not have the number of dimensions that this
+     *             mapper expects.
+     */
+    public void fromObject(double[] state, OBJECT object);
+
+    /**
+     * <p>
+     * Using this mapping to convert (part of) a state-space vector to an object.
+     * </p>
+     * 
+     * @param state
+     *            The state-space vector
+     * @return The representation of (part of) the state-space; not null.
+     * @throws NullPointerException
+     *             If {@code state} is null.
+     * @throws IndexOutOfBoundsException
+     *             If {@code state} does not have the number of dimensions that this
+     *             mapper expects.
+     */
+    public OBJECT toObject(ImmutableVectorN state);
+}
---
+++ src/test/java/uk/badamson/mc/physics/ObjectStateSpaceMapperTest.java
@@ -0,0 +1,40 @@
+package uk.badamson.mc.physics;
+
+import static org.junit.Assert.assertNotNull;
+
+import uk.badamson.mc.ObjectTest;
+import uk.badamson.mc.math.ImmutableVectorN;
+
+/**
+ * <p>
+ * Unit tests for classes that implement the {@link ObjectStateSpaceMapper}
+ * interface.
+ */
+public class ObjectStateSpaceMapperTest {
+
+    public static <OBJECT> void assertInvariants(ObjectStateSpaceMapper<OBJECT> mapper) {
+        // Do nothing
+    }
+
+    public static <OBJECT> void assertInvariants(ObjectStateSpaceMapper<OBJECT> mapper1,
+            ObjectStateSpaceMapper<OBJECT> mapper2) {
+        // Do nothing
+    }
+
+    public static <OBJECT> void fromObject(ObjectStateSpaceMapper<OBJECT> mapper, double[] state, OBJECT object) {
+        mapper.fromObject(state, object);
+
+        assertInvariants(mapper);// check for side-effects
+        ObjectTest.assertInvariants(object);// check for side-effects
+    }
+
+    public static <OBJECT> OBJECT toObject(ObjectStateSpaceMapper<OBJECT> mapper, ImmutableVectorN state) {
+        OBJECT object = mapper.toObject(state);
+
+        assertInvariants(mapper);// check for side-effects
+        assertNotNull("result", object);
+        ObjectTest.assertInvariants(object);
+
+        return object;
+    }
+}
