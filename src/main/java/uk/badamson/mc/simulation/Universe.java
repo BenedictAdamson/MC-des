e26a55ccbfe35ec41b9017997e01c8f16c451c86
Benedict Adamson <badamson@spamcop.net>
2018-09-09 11:10:04 +0100

    Universe CONTAINS objectStates map.

--- src/main/java/uk/badamson/mc/simulation/Universe.java
+++ src/main/java/uk/badamson/mc/simulation/Universe.java
@@ -1,5 +1,8 @@
 package uk.badamson.mc.simulation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <p>
  * A collection of simulated objects and their {@linkplain ObjectState state}
@@ -7,5 +10,40 @@ package uk.badamson.mc.simulation;
  * </p>
  */
 public class Universe {
+
+    private final Map<ObjectStateId, ObjectState> objectStates = new HashMap<>();
+
+    /**
+     * <p>
+     * Construct an empty universe.
+     * </p>
+     * <ul>
+     * <li>The map of IDs to object states {@linkplain Map#isEmpty() is empty}.</li>
+     * </ul>
+     */
+    public Universe() {
+        // Do nothing
+    }
+
+    /**
+     * <p>
+     * All the states of objects within this universe, indexed by their
+     * {@linkplain ObjectState#getId() IDs}.
+     * </p>
+     * <ul>
+     * <li>Always have a (non null) map of IDs to object states.</li>
+     * <li>The map of IDs to object states does not have a null key.</li>
+     * <li>The map of IDs to object states does not have null values.</li>
+     * <li>The map of IDs to object states maps an ID to an object state that has
+     * the same {@linkplain ObjectState#getId() ID}.</li>
+     * <li>The map of IDs to object states may be immutable, or it may be a copy of
+     * an underlying collection.</li>
+     * </ul>
+     * 
+     * @return the objectStates
+     */
+    public final Map<ObjectStateId, ObjectState> getObjectStates() {
+        return objectStates;
+    }
 
 }
--- src/test/java/uk/badamson/mc/simulation/UniverseTest.java
+++ src/test/java/uk/badamson/mc/simulation/UniverseTest.java
@@ -1,5 +1,13 @@
 package uk.badamson.mc.simulation;
 
+import static org.hamcrest.core.IsSame.sameInstance;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collections;
+import java.util.Map;
+
 import org.junit.Test;
 
 import uk.badamson.mc.ObjectTest;
@@ -13,6 +21,21 @@ public class UniverseTest {
 
     public static void assertInvariants(Universe universe) {
         ObjectTest.assertInvariants(universe);// inherited
+
+        final Map<ObjectStateId, ObjectState> objectStates = universe.getObjectStates();
+
+        assertNotNull("Always have a map of IDs to object states.", objectStates);// guard
+
+        for (Map.Entry<ObjectStateId, ObjectState> entry : objectStates.entrySet()) {
+            final ObjectStateId id = entry.getKey();
+            final ObjectState objectState = entry.getValue();
+            assertNotNull("The map of IDs to object states does not have a null key.", id);// guard
+            assertNotNull("The map of IDs to object states does not have null values.", objectState);// guard
+            ObjectStateIdTest.assertInvariants(id);
+            ObjectStateTest.assertInvariants(objectState);
+            assertThat("The map of IDs to object states maps an ID to an object state that has the same ID.",
+                    objectState.getId(), sameInstance(id));
+        }
     }
 
     public static void assertInvariants(Universe universe1, Universe universe2) {
@@ -24,6 +47,7 @@ public class UniverseTest {
         final Universe universe = new Universe();
 
         assertInvariants(universe);
+        assertEquals("The map of IDs to object states is empty.", Collections.emptyMap(), universe.getObjectStates());
     }
 
 }
