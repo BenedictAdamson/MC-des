f67d9a3bcac2ee0f790b0a42943c3d534b24b295
Benedict Adamson <badamson@spamcop.net>
2018-09-11 23:08:27 +0100

    Create basic ObjectStateDependency.

--- src/main/java/uk/badamson/mc/simulation/ObjectStateDependency.java
+++ src/main/java/uk/badamson/mc/simulation/ObjectStateDependency.java
@@ -1,5 +1,9 @@
 package uk.badamson.mc.simulation;
 
+import java.time.Duration;
+import java.util.Objects;
+import java.util.UUID;
+
 import net.jcip.annotations.Immutable;
 
 /**
@@ -10,5 +14,95 @@ import net.jcip.annotations.Immutable;
  */
 @Immutable
 public final class ObjectStateDependency {
+
+    private final ObjectStateId previousStateTransition;
+    private final Duration when;
+
+    /**
+     * <p>
+     * Construct an object with given attributes.
+     * </p>
+     * <ul>
+     * <li>The {@linkplain #getWhen() point in time at which the depended upon
+     * object had the depended upon state} is the given time-stamp.</li>
+     * <li>The {@linkplain #getPreviousStateTransition() previous state transition}
+     * is the given previous state transition.</li>
+     * </ul>
+     * 
+     * @param when
+     *            The point in time at which the {@linkplain #getDependedUpObject()
+     *            depended upon object} had the depended upon state.
+     * @param previousStateTransition
+     *            The state transition (at or) before the {@linkplain #getWhen()
+     *            point in time} of interest.
+     * @throws NullPointerException
+     *             <ul>
+     *             <li>If {@code when} is null.</li>
+     *             <li>If {@code previousStateTransition} is null.</li>
+     *             </ul>
+     * @throws IllegalArgumentException
+     *             If {@code when} is before the {@linkplain ObjectStateId#getWhen()
+     *             time-stamp} of the {@code previousStateTransition}.
+     */
+    public ObjectStateDependency(Duration when, ObjectStateId previousStateTransition) {
+        this.when = Objects.requireNonNull(when, "when");
+        this.previousStateTransition = Objects.requireNonNull(previousStateTransition, "previousStateTransition");
+        if (when.compareTo(previousStateTransition.getWhen()) < 0) {
+            throw new IllegalArgumentException("when is before time-stamp of teh previous state transition");
+        }
+    }
+
+    /**
+     * <p>
+     * The ID of the depended upon object.
+     * </p>
+     * <ul>
+     * <li>Always have a (non null) depended upon object.</li>
+     * <li>The depended upon object is the {@linkplain ObjectStateId#getObject()
+     * object} of the {@linkplain #getPreviousStateTransition() previous state
+     * transition}.</li>
+     * </ul>
+     * 
+     * @return the object ID.
+     */
+    public final UUID getDependedUpObject() {
+        return null;// FIXME
+    }
+
+    /**
+     * <p>
+     * The state transition of the {@linkplain #getDependedUpObject() depended upon
+     * object} (at or) before the {@linkplain #getWhen() point in time} of interest.
+     * </p>
+     * <ul>
+     * <li>Always have a (non null) previous state transition.</li>
+     * </ul>
+     * 
+     * @return the previous state transition.
+     */
+    public final ObjectStateId getPreviousStateTransition() {
+        return previousStateTransition;
+    }
+
+    /**
+     * <p>
+     * The point in time at which the {@linkplain #getDependedUpObject() depended
+     * upon object} had the depended upon state.
+     * </p>
+     * <ul>
+     * <li>Always have a (non null) time at which the depended upon object had the
+     * depended upon state.</li>
+     * <li>The time at which the depended upon object had the depended upon state is
+     * {@linkplain Duration#compareTo(Duration) at or after} the
+     * {@linkplain ObjectStateId#getWhen() time} of the
+     * {@linkplain #getPreviousStateTransition() previous state transition} of the
+     * depended upon object.</li>
+     * </ul>
+     * 
+     * @return when
+     */
+    public final Duration getWhen() {
+        return when;
+    }
 
 }
--- src/test/java/uk/badamson/mc/simulation/ObjectStateDependencyTest.java
+++ src/test/java/uk/badamson/mc/simulation/ObjectStateDependencyTest.java
@@ -1,5 +1,14 @@
 package uk.badamson.mc.simulation;
 
+import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+
+import java.time.Duration;
+import java.util.UUID;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import uk.badamson.mc.ObjectTest;
@@ -12,23 +21,69 @@ import uk.badamson.mc.ObjectTest;
  */
 public class ObjectStateDependencyTest {
 
+    private static final UUID OBJECT_A = ObjectStateIdTest.OBJECT_A;
+    private static final UUID OBJECT_B = ObjectStateIdTest.OBJECT_B;
+    private static final Duration WHEN_1 = Duration.ZERO;
+    private static final Duration WHEN_2 = Duration.ofSeconds(13);
+    private static final Duration WHEN_3 = Duration.ofSeconds(15);
+    private static final UUID VERSION_A = ObjectStateIdTest.VERSION_A;
+    private static final UUID VERSION_B = ObjectStateIdTest.VERSION_B;
+
     public static void assertInvariants(final ObjectStateDependency dependency) {
         ObjectTest.assertInvariants(dependency);// inherited
-    }
 
+        final ObjectStateId previousStateTransition = dependency.getPreviousStateTransition();
+        final Duration when = dependency.getWhen();
+
+        assertNotNull("Always have a previous state transition.", previousStateTransition);// guard
+        assertNotNull("Always have a time at which the depended upon object had the depended upon state.", when);
+
+        ObjectStateIdTest.assertInvariants(previousStateTransition);
+
+        assertThat(
+                "The time at which the depended upon object had the depended upon state is "
+                        + "at or after the time of the previous state transition of the depended upon object.",
+                when, greaterThanOrEqualTo(previousStateTransition.getWhen()));
+    }
     public static void assertInvariants(final ObjectStateDependency dependency1,
             final ObjectStateDependency dependency2) {
         ObjectTest.assertInvariants(dependency1, dependency2);// inherited
+
     }
 
-    private static void constructor() {
-        final ObjectStateDependency dependency = new ObjectStateDependency();
+    private static void constructor(Duration when, ObjectStateId previousStateTransition) {
+        final ObjectStateDependency dependency = new ObjectStateDependency(when, previousStateTransition);
 
         assertInvariants(dependency);
+        assertSame(
+                "The point in time at which the depended upon object had the depended upon state is the given time-stamp.",
+                when, dependency.getWhen());
+        assertSame("The previous state transition is the given previous state transition.", previousStateTransition,
+                dependency.getPreviousStateTransition());
+    }
+
+    private ObjectStateId objectStateId1;
+
+    private ObjectStateId objectStateId2;
+
+    @Test
+    public void constructor_A() {
+        constructor(WHEN_2, objectStateId1);
     }
 
     @Test
-    public void constructor_0() {
-        constructor();
+    public void constructor_B() {
+        constructor(WHEN_3, objectStateId2);
+    }
+
+    @Test
+    public void constructor_C() {
+        constructor(objectStateId1.getWhen(), objectStateId1);
+    }
+
+    @Before
+    public void setUp() {
+        objectStateId1 = new ObjectStateId(OBJECT_A, WHEN_1, VERSION_A);
+        objectStateId2 = new ObjectStateId(OBJECT_B, WHEN_2, VERSION_B);
     }
 }
