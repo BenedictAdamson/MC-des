e4a2409b3a334785ef0bd34f36fe56a696579702
Benedict Adamson <badamson@spamcop.net>
2018-10-07 01:42:42 +0100

    ValueHistory contains transitionTimes.

--- src/main/java/uk/badamson/mc/simulation/ValueHistory.java
+++ src/main/java/uk/badamson/mc/simulation/ValueHistory.java
@@ -1,7 +1,9 @@
 package uk.badamson.mc.simulation;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Objects;
+import java.util.SortedSet;
 
 import net.jcip.annotations.Immutable;
 import net.jcip.annotations.NotThreadSafe;
@@ -17,14 +19,33 @@ import net.jcip.annotations.NotThreadSafe;
  */
 @NotThreadSafe
 public final class ValueHistory<VALUE> {
+    /**
+     * <p>
+     * The smallest (most negative) {@link Duration} value.
+     * </p>
+     */
+    public static final Duration START_OF_TIME = Duration.ofSeconds(Long.MIN_VALUE);
+
+    /**
+     * <p>
+     * The largest (most positive) {@link Duration} value.
+     * </p>
+     */
+    public static final Duration END_OF_TIME = Duration.ofSeconds(Long.MAX_VALUE, 999_999_999);;
 
     /**
      * <p>
      * Construct an value history that is null for all points in time.
      * </p>
+     * <ul>
+     * <li>The {@linkplain #get(Duration) value} of this history at the
+     * {@linkplain #START_OF_TIME start of time} is null.</li>
+     * <li>This {@linkplain SortedSet#isEmpty() has no}
+     * {@linkplain #getTransitionTimes() transition times}.</li>
+     * </ul>
      */
     public ValueHistory() {
-        // FIXME
+        // TODO
     }
 
     /**
@@ -42,5 +63,28 @@ public final class ValueHistory<VALUE> {
     public VALUE get(Duration t) {
         Objects.requireNonNull(t, "t");
         return null;// TODO
+    }
+
+    /**
+     * <p>
+     * The points in time when the value of this history changes.
+     * </p>
+     * <ul>
+     * <li>Always have a (non null) set of transition times.</li>
+     * <li>There is not a transition at the {@linkplain ValueHistory#START_OF_TIME
+     * start of time}.</li>
+     * <li>For all points in time in the set of transition times, the
+     * {@linkplain #get(Duration) value} just before the transition is not equal to
+     * the value at the transition.</li>
+     * <li>For all points in time not in the set of transition times (except the
+     * {@linkplain ValueHistory#START_OF_TIME start of time}), the
+     * {@linkplain #get(Duration) value} just before the point in time is equal to
+     * the value at the point in time.</li>
+     * </ul>
+     * 
+     * @return the transition times
+     */
+    public SortedSet<Duration> getTransitionTimes() {
+        return Collections.emptySortedSet();// TODO
     }
 }
--- src/test/java/uk/badamson/mc/simulation/ValueHistoryTest.java
+++ src/test/java/uk/badamson/mc/simulation/ValueHistoryTest.java
@@ -1,8 +1,15 @@
 package uk.badamson.mc.simulation;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.time.Duration;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.SortedSet;
 
 import org.junit.Test;
 
@@ -20,10 +27,35 @@ public class ValueHistoryTest {
 
     public static <VALUE> void assertInvariants(ValueHistory<VALUE> history) {
         ObjectTest.assertInvariants(history);// inherited
+
+        assertTransitionTimesInvariants(history);
+    }
+
+    private static <VALUE> void assertInvariants(ValueHistory<VALUE> history, Duration time) {
+        final SortedSet<Duration> transitionTimes = history.getTransitionTimes();
+        assertTrue(
+                "For all points in time not in the set of transition times (except the start of time), the value just before the point in time is equal to the value at the point in time.",
+                transitionTimes.contains(time) || time.equals(ValueHistory.START_OF_TIME)
+                        || Objects.equals(history.get(time.minusNanos(1L)), history.get(time)));
     }
 
     public static <VALUE> void assertInvariants(ValueHistory<VALUE> history1, ValueHistory<VALUE> history2) {
         ObjectTest.assertInvariants(history1, history2);// inherited
+    }
+
+    private static <VALUE> SortedSet<Duration> assertTransitionTimesInvariants(ValueHistory<VALUE> history) {
+        final SortedSet<Duration> transitionTimes = history.getTransitionTimes();
+
+        assertNotNull("Always have a set of transition times.", transitionTimes);// guard
+        for (Duration transitionTime : transitionTimes) {
+            assertNotEquals("There is not a transition at the start of time.", ValueHistory.START_OF_TIME,
+                    transitionTime);// guard
+            assertNotEquals(
+                    "For all points in time in the set of transition times, the value just before the transition is not equal to the value at the transition.",
+                    history.get(transitionTime.minusNanos(1L)), history.get(transitionTime));
+        }
+
+        return transitionTimes;
     }
 
     @Test
@@ -31,8 +63,10 @@ public class ValueHistoryTest {
         final var history = new ValueHistory<Integer>();
 
         assertInvariants(history);
-        assertNull("Value history is null for all points in time [1]", history.get(DURATION_1));
-        assertNull("Value history is null for all points in time [2]", history.get(DURATION_2));
+        assertInvariants(history, DURATION_1);
+        assertInvariants(history, DURATION_2);
+        assertNull("The value of this history at the start of time is null.", history.get(ValueHistory.START_OF_TIME));
+        assertEquals("This has no transition times.", Collections.EMPTY_SET, history.getTransitionTimes());
     }
 
 }
