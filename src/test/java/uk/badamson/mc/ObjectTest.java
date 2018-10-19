82ad2dad20cba37a5fad4e4d8efb59d18a492e4f
Benedict Adamson <badamson@spamcop.net>
2018-10-19 23:39:23 +0100

    Experiment with grouped assertions.

--- src/test/java/uk/badamson/mc/ObjectTest.java
+++ src/test/java/uk/badamson/mc/ObjectTest.java
@@ -11,15 +11,16 @@ public final class ObjectTest {
 
     public static void assertInvariants(Object object) {
         assert object != null;
-        assertEquals(object, object, "An object is always equivalent to itself");
-        assertFalse(object.equals(null), "An object is never equivalent to null");
+        assertAll("equals", () -> assertEquals(object, object, "An object is always equivalent to itself"),
+                () -> assertFalse(object.equals(null), "An object is never equivalent to null"));
     }
 
     public static void assertInvariants(Object object1, Object object2) {
         assert object1 != null;
         assert object2 != null;
         final boolean equals = object1.equals(object2);
-        assertEquals(equals, object2.equals(object1), "Equality is symmetric");
-        assertFalse(equals && object1.hashCode() != object2.hashCode(), "hashCode() is consistent with equals()");
+        assertAll("equals", () -> assertEquals(equals, object2.equals(object1), "Equality is symmetric"),
+                () -> assertFalse(equals && object1.hashCode() != object2.hashCode(),
+                        "hashCode() is consistent with equals()"));
     }
 }
