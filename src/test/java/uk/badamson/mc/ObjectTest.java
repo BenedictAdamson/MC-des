73deeede7f50502addaf08bd51479422b3405fe1
Benedict Adamson <badamson@spamcop.net>
2018-10-19 23:40:30 +0100

    Experiment with grouped assertions.

--- src/test/java/uk/badamson/mc/ObjectTest.java
+++ src/test/java/uk/badamson/mc/ObjectTest.java
@@ -1,6 +1,8 @@
 package uk.badamson.mc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * <p>
