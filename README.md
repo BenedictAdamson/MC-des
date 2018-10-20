07453de40a7600c91f3fd806a8b252f8724a754f
Benedict Adamson <badamson@spamcop.net>
2018-10-21 00:39:59 +0100

    Add license notices.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # MC-des
 Multi-threaded Discrete Event Simulation (DES) engine
+
+## License
+
+© Copyright Benedict Adamson 2018.
+
+Licensed using the [GNU GENERAL PUBLIC LICENSE](https://www.gnu.org/licenses/gpl.html) Version 3.
+![GPLV3](https://www.gnu.org/graphics/gplv3-with-text-136x68.png)
+
+## Technologies Used
+
+* Java 10
+* JUnit 5
+* Maven
+* JCIP annotations
+* Git
--- src/main/java/uk/badamson/mc/simulation/ValueHistory.java
+++ src/main/java/uk/badamson/mc/simulation/ValueHistory.java
@@ -1,4 +1,22 @@
 package uk.badamson.mc.simulation;
+/* 
+ * © Copyright Benedict Adamson 2018.
+ * 
+ * This file is part of MC-des.
+ *
+ * MC-des is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC-des is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MC-des.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import java.time.Duration;
 import java.util.Collections;
