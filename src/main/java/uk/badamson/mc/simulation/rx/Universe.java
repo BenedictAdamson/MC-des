090d9750472ffd0de072b1d50a1af63bf4480771
Benedict Adamson <badamson@spamcop.net>
2021-05-01 15:32:16 +0100

    Add (dummy) rx.Universe<STATE>

---
+++ src/main/java/uk/badamson/mc/simulation/rx/Universe.java
@@ -0,0 +1,48 @@
+package uk.badamson.mc.simulation.rx;
+/*
+ * © Copyright Benedict Adamson 2018m2021.
+ *
+ * This file is part of MC-des.
+ *
+ * MC-des is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC-des is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MC-des.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import javax.annotation.concurrent.Immutable;
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * <p>
+ * A collection of simulated objects and their {@linkplain ObjectHistory
+ * histories}.
+ * </p>
+ * <p>
+ * The histories of the objects may be <dfn>asynchronous</dfn>: different
+ * objects may have state transitions at different times.</li>
+ * <p>
+ * This collection is modifiable: the histories of the simulated objects may be
+ * appended to. This collection enforces constraints that ensure that the object
+ * histories are <dfn>consistent</dfn>. Consistency means that if a universe
+ * contains an object state, it also contains all the depended upon states of
+ * that state.
+ * </p>
+ *
+ * @param <STATE>
+ *            The class of states of the simulated objects. This must be
+ *            {@link Immutable immutable}. It ought to have value semantics, but
+ *            that is not required.
+ */
+@ThreadSafe
+public final class Universe<STATE> {
+
+}
---
+++ src/test/java/uk/badamson/mc/simulation/rx/UniverseTest.java
@@ -0,0 +1,35 @@
+package uk.badamson.mc.simulation.rx;
+/*
+ * © Copyright Benedict Adamson 2018.
+ *
+ * This file is part of MC-des.
+ *
+ * MC-des is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC-des is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MC-des.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import javax.annotation.Nonnull;
+
+import uk.badamson.mc.ObjectTest;
+
+public class UniverseTest {
+
+    public static <STATE> void assertInvariants(@Nonnull final Universe<STATE> universe) {
+        ObjectTest.assertInvariants(universe);// inherited
+    }
+
+    public static <STATE> void assertInvariants(@Nonnull final Universe<STATE> universe1,
+            @Nonnull final Universe<STATE> universe2) {
+        ObjectTest.assertInvariants(universe1, universe2);// inherited
+    }
+}
