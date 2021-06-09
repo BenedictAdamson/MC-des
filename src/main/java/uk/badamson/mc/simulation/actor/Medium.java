b044ae7a6689f59fa73bc693aef36e8f7fd7d1e6
Benedict Adamson <badamson@spamcop.net>
2021-06-09 22:37:02 +0100

    Medium<STATE>

---
+++ src/main/java/uk/badamson/mc/simulation/actor/Medium.java
@@ -0,0 +1,96 @@
+package uk.badamson.mc.simulation.actor;
+/*
+ * © Copyright Benedict Adamson 2021.
+ *
+ * This file is part of MC-des.
+ *
+ * MC-des is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC-des is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MC-des.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import java.util.Collection;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * <p>
+ * A means for transmitting {@linkplain Signal signals} from their
+ * {@linkplain Signal#getSender() senders} to their
+ * {@linkplain Signal#getReceiver() receivers}
+ * </p>
+ *
+ * @see Signal
+ */
+@ThreadSafe
+interface Medium<STATE> {
+
+    /**
+     * <p>
+     * Add several signals to be transmitted through this medium.
+     * </p>
+     * <p>
+     * {@linkplain Set#addAll(Collection) Adds all} the given {@code signals} to the
+     * {@linkplain #getSignals() signals} being transmitted through this medium.
+     * </p>
+     *
+     * @param signals
+     *            The signals to be transmitted. The method will not modify this
+     *            collection.
+     * @throws NullPointerException
+     *             <ul>
+     *             <li>If {@code signals} is null</li>
+     *             <li>If {@code signals} contains null</li>
+     *             </ul>
+     * @see Set#addAll(Collection)
+     */
+    void addAll(@Nonnull Collection<Signal<STATE>> signals);
+
+    /**
+     * <p>
+     * The signals that are being transmitted through this medium.
+     * </p>
+     * <ul>
+     * <li>Does not contain a null signal.</li>
+     * <li>May be unmodifiable.</li>
+     * <li>A snapshot: the returned set does not reflect subsequent changes.</li>
+     * </ul>
+     */
+    @Nonnull
+    Set<Signal<STATE>> getSignals();
+
+    /**
+     * <p>
+     * Remove several signals, which might previously have been
+     * {@linkplain #addAll(Collection) added} for transmission through this medium.
+     * </p>
+     * <p>
+     * {@linkplain Set#removeAll(Collection) Removes all} the given {@code signals}
+     * from the {@linkplain #getSignals() set of signals} being transmitted through
+     * this medium.
+     * </p>
+     *
+     * @param signals
+     *            The signals to be removed. The method will not modify this
+     *            collection.
+     * @throws NullPointerException
+     *             <ul>
+     *             <li>If {@code signals} is null</li>
+     *             <li>If {@code signals} contains null</li>
+     *             </ul>
+     * @see Set#removeAll(Collection)
+     */
+    void removeAll(@Nonnull Collection<Signal<STATE>> signals);
+
+}
---
+++ src/test/java/uk/badamson/mc/simulation/actor/MediumTest.java
@@ -0,0 +1,88 @@
+package uk.badamson.mc.simulation.actor;
+/*
+ * © Copyright Benedict Adamson 2018.
+ *
+ * This file is part of MC-des.
+ *
+ * MC-des is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC-des is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MC-des.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.annotation.Nonnull;
+
+import uk.badamson.dbc.assertions.CollectionTest;
+
+public class MediumTest {
+
+    static final class RecordingMedium<STATE> implements Medium<STATE> {
+        private final Set<Signal<STATE>> signals = ConcurrentHashMap.newKeySet();
+
+        @Override
+        public void addAll(final Collection<Signal<STATE>> signals) {
+            this.signals.addAll(signals);
+        }
+
+        @Override
+        public Set<Signal<STATE>> getSignals() {
+            return Set.copyOf(signals);
+        }
+
+        @Override
+        public void removeAll(final Collection<Signal<STATE>> signals) {
+            this.signals.removeAll(signals);
+        }
+
+    }// class
+
+    static <STATE> void addAll(@Nonnull final Medium<STATE> medium, @Nonnull final Collection<Signal<STATE>> signals) {
+        final Set<Signal<STATE>> mediumSignals0 = medium.getSignals();
+
+        medium.addAll(signals);
+
+        assertInvariants(medium);
+        final Set<Signal<STATE>> mediumSignals = medium.getSignals();
+        assertAll("signals contains all",
+                () -> assertThat("original signals", mediumSignals.containsAll(mediumSignals0)),
+                () -> assertThat("added signals", mediumSignals.containsAll(signals)));
+    }
+
+    static <STATE> void assertInvariants(@Nonnull final Medium<STATE> medium) {
+        final Set<Signal<STATE>> signals = medium.getSignals();
+        assertThat("signals", signals, notNullValue());// guard
+        CollectionTest.assertForAllElements("signals", signals, signal -> {
+            assertThat("signal", signal, notNullValue());// guard
+            SignalTest.assertInvariants(signal);
+        });
+    }
+
+    static <STATE> void removeAll(@Nonnull final Medium<STATE> medium,
+            @Nonnull final Collection<Signal<STATE>> signals) {
+        final Set<Signal<STATE>> mediumSignals0 = medium.getSignals();
+
+        medium.removeAll(signals);
+
+        assertInvariants(medium);
+        final Set<Signal<STATE>> mediumSignals = medium.getSignals();
+        assertAll("signals", () -> assertThat("does not have added signals", mediumSignals0.containsAll(mediumSignals)),
+                () -> assertThat("has none of the removed signals", Collections.disjoint(mediumSignals, signals)));
+    }
+}
