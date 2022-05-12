0910e154f05a1ad2be1ec918afce653b462cd009
Benedict Adamson <badamson@spamcop.net>
2022-05-12 21:46:14 +0100

    Universe thread safety for addition and removal of actors.

--- src/main/java/uk/badamson/mc/simulation/actor/Universe.java
+++ src/main/java/uk/badamson/mc/simulation/actor/Universe.java
@@ -19,6 +19,7 @@ package uk.badamson.mc.simulation.actor;
  */
 
 import javax.annotation.Nonnull;
+import javax.annotation.concurrent.GuardedBy;
 import javax.annotation.concurrent.Immutable;
 import javax.annotation.concurrent.ThreadSafe;
 import java.time.Duration;
@@ -30,6 +31,9 @@ import java.util.concurrent.Future;
 /**
  * <p>
  * A collection of {@linkplain Actor simulated objects and their histories}.
+ * </p>
+ * <p>
+ * This collection is modifiable.
  * </p>
  * <p>
  * The histories of the objects may be <dfn>asynchronous</dfn>: different
@@ -47,12 +51,22 @@ import java.util.concurrent.Future;
 @ThreadSafe
 public final class Universe<STATE> implements Collection<Actor<STATE>> {
 
+    private final Object additionLock = new Object();
+
     private final Map<UUID, Actor<STATE>> actors = new ConcurrentHashMap<>();
 
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * The collection is not modifiable through the iterator.
+     * That is, {@link Iterator#remove()} throws an {@link UnsupportedOperationException}.
+     * </p>
+     */
     @Nonnull
     @Override
     public Iterator<Actor<STATE>> iterator() {
-        return actors.values().iterator();
+        return Collections.unmodifiableCollection(actors.values()).iterator();
     }
 
     @Override
@@ -60,15 +74,35 @@ public final class Universe<STATE> implements Collection<Actor<STATE>> {
         return actors.size();
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * This collection may not contain null elements;
+     * this method will therefore throw a {@link NullPointerException}
+     * if {@code actor} is null
+     * </p>
+     *
+     * @throws NullPointerException if {@code actor} is null
+     */
     @Override
     public boolean add(final Actor<STATE> actor) {
         Objects.requireNonNull(actor);
+        synchronized (additionLock) {
+            return addWhileLocked(actor);
+        }
+    }
+
+    @GuardedBy("additionLock")
+    private boolean addWhileLocked(@Nonnull final Actor<STATE> actor) {
         return actors.putIfAbsent(actor.lock, actor) == null;
     }
 
     @Override
     public void clear() {
-        actors.clear();
+        synchronized (additionLock) {
+            actors.clear();
+        }
     }
 
     @Override
@@ -110,20 +144,31 @@ public final class Universe<STATE> implements Collection<Actor<STATE>> {
         if (!(o instanceof Actor<?>)) {
             return false;
         }
-        return removeActor((Actor<?>) o);
+        synchronized (additionLock) {
+            return removeWhileLocked((Actor<?>) o);
+        }
     }
 
     @Override
-    public boolean removeAll(final Collection<?> objects) {
+    public boolean removeAll(@Nonnull final Collection<?> objects) {
+        synchronized (additionLock) {
+            return removeAllWhileLocked(objects);
+        }
+    }
+
+    @GuardedBy("additionLock")
+    @Nonnull
+    private Boolean removeAllWhileLocked(final Collection<?> objects) {
         return objects.stream()
                 .filter(o -> o instanceof Actor<?>)
                 .map(o -> (Actor<?>) o)
-                .map(this::removeActor)
+                .map(this::removeWhileLocked)
                 .reduce((r1, r2) -> r1 || r2)
                 .orElse(Boolean.FALSE);
     }
 
-    private boolean removeActor(final Actor<?> actor) {
+    @GuardedBy("additionLock")
+    private boolean removeWhileLocked(final Actor<?> actor) {
         return actors.remove(actor.lock) != null;
     }
 
@@ -146,15 +191,24 @@ public final class Universe<STATE> implements Collection<Actor<STATE>> {
 
     @Override
     public boolean addAll(@Nonnull final Collection<? extends Actor<STATE>> c) {
+        synchronized (additionLock) {
+            return addAllWhileLocked(c);
+        }
+    }
+
+    @GuardedBy("additionLock")
+    private boolean addAllWhileLocked(@Nonnull final Collection<? extends Actor<STATE>> c) {
         return c.stream()
-                .map(this::add)
+                .map(this::addWhileLocked)
                 .reduce((a1, a2) -> a1 || a2)
                 .orElse(false);
     }
 
     @Override
     public boolean retainAll(@Nonnull final Collection<?> c) {
-        return actors.values().retainAll(c);
+        synchronized (additionLock) {
+            return actors.values().retainAll(c);
+        }
     }
 
     /**
@@ -163,6 +217,11 @@ public final class Universe<STATE> implements Collection<Actor<STATE>> {
      * until the {@link  Actor#getWhenReceiveNextSignal time of their next signal}
      * is {@link Duration#compareTo(Duration) at or after} a given time,
      * with the processing done using a given {@link Executor}.
+     * </p>
+     * <p>
+     * Although this class is threadsafe, calling this method from multiple threads is unwise.
+     * In that case it is possible for {@linkplain Actor.AffectedActors#getAdded() additions} and
+     * {@linkplain Actor.AffectedActors#getRemoved() removals} to be done in a manner that leads to lost additions.
      * </p>
      *
      * @return a Future that {@linkplain Future#isDone() is done} when all the actors have been advanced,
@@ -182,8 +241,10 @@ public final class Universe<STATE> implements Collection<Actor<STATE>> {
 
     @Nonnull
     private Actor.AffectedActors<STATE> apply(@Nonnull final Actor.AffectedActors<STATE> affectedActors) {
-        removeAll(affectedActors.getRemoved());
-        addAll(affectedActors.getAdded());
+        synchronized (additionLock) {
+            removeAllWhileLocked(affectedActors.getRemoved());
+            addAllWhileLocked(affectedActors.getAdded());
+        }
         return affectedActors;
     }
 }
